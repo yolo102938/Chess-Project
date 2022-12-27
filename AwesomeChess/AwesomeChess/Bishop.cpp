89f22846f4f2c5e369c169b27b36e7b099afabde
unknown <jonathan.ginzburg@gmail.com>
2022-12-27 17:56:01 +0200

    finished queen move

---
+++ AwesomeChess/AwesomeChess/Bishop.cpp
@@ -0,0 +1,28 @@
+#include "Bishop.h"
+bool Bishop::checkMove(string place) 
+{
+	int tempPlaceX = this->getPositionNumber_X();
+	int tempPlaceY = this->getPositionNumber_Y();
+	int destx = Piece::getPositionNumber_X(place);
+	int desty = Piece::getPositionNumber_Y(place);
+	int move = 0;
+	if (abs((desty - tempPlaceY) / (destx - tempPlaceX)) != 1) //if its not diagonal return false
+	{
+		return(false);
+	}
+	if (desty - tempPlaceY > 0) {
+		move = -1;
+	}
+	else {
+		move = 1;
+	}
+	while(destx!=tempPlaceX)//no need to check both cus it has the same number of moves
+	{
+		if (!_b->isEmpty(tempPlaceX, tempPlaceY)) {
+			return(false);
+		}
+		tempPlaceX=+move;
+		tempPlaceY=+move;
+	}
+	return(true);
+}
---
+++ AwesomeChess/AwesomeChess/Queen.cpp
@@ -0,0 +1,51 @@
+#include "Queen.h"
+bool Queen::checkMove(string place) 
+{
+	int tempPlaceX = this->getPositionNumber_X();
+	int tempPlaceY = this->getPositionNumber_Y();
+	int destx = Piece::getPositionNumber_X(place);
+	int desty = Piece::getPositionNumber_Y(place);
+
+	//up and down chckers
+	if (destx == tempPlaceX) //if the path is the same col
+	{
+		for (tempPlaceY; tempPlaceY < desty; desty > tempPlaceY ? tempPlaceY++ : tempPlaceY--) //check there is something that is blocking, up or down depends on the dst
+		{
+			if (!_b->isEmpty(tempPlaceX, tempPlaceY)) {
+				return(false);
+			}
+		}
+		return(true);
+	}
+	else if (desty == tempPlaceY) //if the path is the same row
+	{
+		for (tempPlaceX; tempPlaceX < destx; destx > tempPlaceX ? tempPlaceX++ : tempPlaceX--) //check there is something that is blocking, left or right depends on the dst
+		{
+			if (!_b->isEmpty(tempPlaceX, tempPlaceY)) {
+				return(false);
+			}
+		}
+		return(true);
+	}
+	//if not up and down, check diagonal. if not, return false
+	int move = 0;
+	if (abs((desty - tempPlaceY) / (destx - tempPlaceX)) == 1)
+	{
+		if (desty - tempPlaceY > 0) {
+			move = -1;
+		}
+		else {
+			move = 1;
+		}
+		while (destx != tempPlaceX)//no need to check both cus it has the same number of moves
+		{
+			if (!_b->isEmpty(tempPlaceX, tempPlaceY)) {
+				return(false);
+			}
+			tempPlaceX = +move;
+			tempPlaceY = +move;
+		}
+	}
+	return(false);
+	
+}
